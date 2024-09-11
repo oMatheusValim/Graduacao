bb2dd9922b9a6aa75d3740be433e02980cfe10d5
Valim <134014256+oMatheusValim@users.noreply.github.com>
2024-09-11 16:44:43 -0300

    Update Ordenação de Números.py

--- SSC0800 - BECD 2024/Lista 1/Ordenação de Números.py
+++ SSC0800 - BECD 2024/Lista 1/Ordenação de Números.py
@@ -1,17 +1,16 @@
-'''Desenvolva um código em Python que leia uma lista de números inteiros e escreva a lista em ordem crescente.
+# Desenvolva um código em Python que leia uma lista de números inteiros e escreva a lista em ordem crescente.
 
-ENTRADA
-A entrada será uma sequência de números inteiros separados por espaço
+# ENTRADA
+# A entrada será uma sequência de números inteiros separados por espaço
 
-5 3 8 6 2 
+# 5 3 8 6 2 
 
-SAÍDA
-A saída será a lista de números inteiros em ordem crescente, separados por espaço.
+# SAÍDA
+# A saída será a lista de números inteiros em ordem crescente, separados por espaço.
 
-2 3 5 6 8'''
+# 2 3 5 6 8
+
 num = input()
 num_list = list(map(int, num.split()))
 num_list.sort()
 print(" ".join(map(str, num_list)))
-
-# FEITO

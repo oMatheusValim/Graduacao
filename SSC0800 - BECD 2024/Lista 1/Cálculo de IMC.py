e27d283a65bb3c1595ffcc0cb6006909a4f134a2
Valim <134014256+oMatheusValim@users.noreply.github.com>
2024-09-11 16:43:22 -0300

    Update Cálculo de IMC.py

--- SSC0800 - BECD 2024/Lista 1/Cálculo de IMC.py
+++ SSC0800 - BECD 2024/Lista 1/Cálculo de IMC.py
@@ -1,14 +1,14 @@
-'''Desenvolva um programa em pýthon que leia a altura (metros) e a massa (kg) de uma certa quantidade de pessoa, calcule e escreva o Índice de Massa Corporal (IMC) de cada uma delas.
-IMC = m/(altura)^2 massa dividida pela altura ao quadrado
+# Desenvolva um programa em pýthon que leia a altura (metros) e a massa (kg) de uma certa quantidade de pessoa, calcule e escreva o Índice de Massa Corporal (IMC) de cada uma delas.
+# IMC = m/(altura)^2 massa dividida pela altura ao quadrado
 
-ENTRADA(altura,peso): 
-1.75,80.3
-1.63,76.5 
-1.91,93.3
-2.03,85.4
+# ENTRADA(altura,peso): 
+# 1.75,80.3
+# 1.63,76.5 
+# 1.91,93.3
+# 2.03,85.4
 
-SAÍDA:
-26.22 28.79 25.57 20.72''' 
+# SAÍDA:
+# 26.22 28.79 25.57 20.72
  
 repeticoes = 0
 altura_peso = []
@@ -26,5 +26,3 @@ for i in range(len(altura_peso)):
     list_imc.append('{:.2f}'.format(imc))
 texto = ' '.join(list_imc)
 print(texto)
-
-# FEITO

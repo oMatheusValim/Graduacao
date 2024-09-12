cdfebe9c5be71d77a8f71fdb455ebddfbf6f608e
Valim <134014256+oMatheusValim@users.noreply.github.com>
2024-09-11 21:47:58 -0300

    Update Distância cartesiana.py

--- SSC0800 - BECD 2024/Lab 2/Distância cartesiana.py
+++ SSC0800 - BECD 2024/Lab 2/Distância cartesiana.py
@@ -1,10 +1,7 @@
-# Crie uma função que receba as coordenadas de 2 pontos no plano cartesiano (x,y) e retorne a distância entre eles. Crie um programa que lê as coordenadas, executa a função e imprime a distância.
 import math
-d1 = input().split(",")
-d2 = input().split(",")
-x1 = int(d1[0])
-y1 = int(d1[1])
-x2 = int(d2[0])
-y2 = int(d2[1])
+x1 = float( input().strip())
+y1  = float( input().strip())
+x2 = float( input().strip())
+y2 =  float( input().strip())
 distância = math.sqrt((x1-x2)**2+(y1-y2)**2)
-print(distância)
+print('%.6f' %(distância))

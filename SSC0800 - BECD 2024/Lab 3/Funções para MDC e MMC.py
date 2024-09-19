db390bd9185280a2d0d3c04db5b5d18172487739
Valim <134014256+oMatheusValim@users.noreply.github.com>
2024-09-18 22:52:35 -0300

    Update Funções para MDC e MMC.py

--- SSC0800 - BECD 2024/Lab 3/Funções para MDC e MMC.py
+++ SSC0800 - BECD 2024/Lab 3/Funções para MDC e MMC.py
@@ -1,3 +1,22 @@
 # Crie duas funções que recebam 3 números inteiros. Uma delas deve retornar o máximo divisor comum (m.d.c.) e a outra, o mínimo múltiplo comum (m.m.c.) desses números. 
 
 # Crie um programa que leia 3 números inteiros, execute as duas funções e mostre o mmc e o mdc desses números.
+
+import math
+
+def mdc_de_tres(n1, n2, n3):
+    return math.gcd(math.gcd(n1, n2), n3)
+
+def mmc(a, b):
+    return abs(a * b) // math.gcd(a, b)
+
+def mmc_de_tres(n1, n2, n3):
+    return mmc(mmc(n1, n2), n3)
+
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+
+multiplO_comum = mmc_de_tres(n1, n2, n3)
+divisor_comum = mdc_de_tres(n1, n2, n3)
+print(f"{divisor_comum} {multiplO_comum}")

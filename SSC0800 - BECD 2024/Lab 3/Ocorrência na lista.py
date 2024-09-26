6209b51ed03d62fefcd876563090895b48217dfe
Valim <134014256+oMatheusValim@users.noreply.github.com>
2024-09-25 22:03:04 -0300

    Update Ocorrência na lista.py

--- SSC0800 - BECD 2024/Lab 3/Ocorrência na lista.py
+++ SSC0800 - BECD 2024/Lab 3/Ocorrência na lista.py
@@ -1,3 +1,14 @@
 # Crie uma função que recebe um inteiro N e uma lista L com 10 inteiros. A Função deve retornar o número de vezes que N aparece na lista L. 
 
 # Crie um programa que lê o inteiro N e a lista L, execute a função e imprima o resultado.
+
+lista = []
+while len(lista) < 11:
+    entrada = int(input().strip())
+    lista.append(entrada)
+numero = lista[0]
+cont = -1
+for i in lista:
+    if i == numero:
+        cont += 1
+print(cont)

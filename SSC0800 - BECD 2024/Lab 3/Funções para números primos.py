80dda3895f797442fb9d4a888fa3eb24bdbd0129
Valim <134014256+oMatheusValim@users.noreply.github.com>
2024-09-25 21:49:36 -0300

    Update Funções para números primos.py

--- SSC0800 - BECD 2024/Lab 3/Funções para números primos.py
+++ SSC0800 - BECD 2024/Lab 3/Funções para números primos.py
@@ -1,3 +1,19 @@
 # Criar um programa que lê um número e gerar todos os números primos entre 2 e o número fornecido pelo usuário, escrevendo na tela o resultado.
 
-# Obs: criar uma função que verifica se um número é primo para uso no programa. Se o número for primo, a função retorna 1. Caso contrário, retorna 0
+import math
+
+def primo(n):
+    primos = []
+    for i in range(2, n+1):
+        primo_sim = True
+        for p in range(2, int(math.sqrt(i) + 1)):
+            if i % p == 0:
+                primo_sim = False
+                break
+        if primo_sim:
+            primos.append(i)
+    return primos
+
+limite = int(input())
+veri = primo(limite)
+print("\n".join(map(str, veri)))

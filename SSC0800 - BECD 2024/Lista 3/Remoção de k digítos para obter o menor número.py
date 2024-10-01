1b3946ffdc523ae16ac2ad830d66a3957cec0185
Valim <134014256+oMatheusValim@users.noreply.github.com>
2024-10-01 11:17:52 -0300

    Create Remoção de k digítos para obter o menor número.py

---
+++ SSC0800 - BECD 2024/Lista 3/Remoção de k digítos para obter o menor número.py
@@ -0,0 +1,23 @@
+# Dada uma string numérica num e um número inteiro k, remova exatamente k dígitos da string para formar o menor número possível. A função deve retornar a menor string numérica possível após a remoção. Se o número resultante tiver zeros à esquerda, remova-os.
+
+# Entrada
+# 1432219 3
+
+# Saída
+# 1219
+
+def remover_k(n1, n2):
+    pilha = []
+    for dig in n1:
+        while n2 > 0 and pilha and pilha[-1] > dig:
+            pilha.pop()
+            n2 -= 1
+        pilha.append(dig)
+    pilha = pilha[:-n2] if n2 > 0 else pilha
+    return ''.join(pilha).lstrip('0') or '0'
+
+entrada = input().split()
+inteiro_k = int(entrada[-1])
+numero_original = entrada[0]
+result = remover_k(numero_original, inteiro_k)
+print(result)

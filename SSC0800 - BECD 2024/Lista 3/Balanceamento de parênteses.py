85711cb6e8f6b3f82c7e66cdb323bc6ab6fea8c4
Valim <134014256+oMatheusValim@users.noreply.github.com>
2024-10-01 11:15:45 -0300

    Create Balanceamento de parênteses.py

---
+++ SSC0800 - BECD 2024/Lista 3/Balanceamento de parênteses.py
@@ -0,0 +1,20 @@
+# Crie uma função recursiva que verifique se uma string contendo apenas parênteses ( e ) está corretamente balanceada. Uma string está corretamente balanceada se para cada ( há um correspondente ) na ordem correta.
+
+# Entrada
+# "(())"
+
+# Saída
+# True
+
+def veri(n):
+    cont1, cont2 = 0, 0
+    for i in n:
+        if i == "(":
+            cont1 += 1
+        elif i == ")":
+            cont2 += 1
+    if cont1 == cont2:
+        return True
+    return False
+entrada = input().strip()
+print(veri(entrada))

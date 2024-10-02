71d4cae6da41c3e16158d836375902e275f59588
Valim <134014256+oMatheusValim@users.noreply.github.com>
2024-10-02 10:42:28 -0300

    Update Fibonacci reverso.py

--- SSC0800 - BECD 2024/Lab 4/Fibonacci reverso.py
+++ SSC0800 - BECD 2024/Lab 4/Fibonacci reverso.py
@@ -1,2 +1,15 @@
 # Faça uma função recursiva que receba N e retorne o N-ésimo termo da sequência Fibonacci, sendo N>2. Ex: para N = 4, retorno = 2. Para N = 6, retorno = 5.
 
+def fibo(n, memo={}):
+    if n in memo:
+        return memo[n]
+    
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        memo[n] = fibo(n-1, memo) + fibo(n-2, memo)
+        return memo[n]
+N = int(input())
+print(fibo(N-1))

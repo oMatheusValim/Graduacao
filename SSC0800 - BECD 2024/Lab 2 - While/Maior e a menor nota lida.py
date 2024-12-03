fe0aabab29fca4d63b82969c1584539032e4b689
Valim <134014256+oMatheusValim@users.noreply.github.com>
2024-12-03 16:06:13 -0300

    Update Maior e a menor nota lida.py

--- SSC0800 - BECD 2024/Lab 2 - While/Maior e a menor nota lida.py
+++ SSC0800 - BECD 2024/Lab 2 - While/Maior e a menor nota lida.py
@@ -1,4 +1,5 @@
 # Elabore um programa que faça leitura de vários notas, até que se digite um número negativo. O programa deve imprimir a maior e a menor nota lida. Obs: o número negativo digitado não deve contar como nota.
+
 lista = []
 while True:
     entrada = int(input())

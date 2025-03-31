24fd8420b34ed4e69565b92aed946b80f5ccf9c8
Valim <134014256+oMatheusValim@users.noreply.github.com>
2025-03-31 13:19:47 -0300

    correção

--- SSC0601 - Eng Comp 2025/Semana 3/VerificacaoQuadrilatero.cpp
+++ SSC0601 - Eng Comp 2025/Semana 3/VerificacaoQuadrilatero.cpp
@@ -6,7 +6,7 @@ int main(){
     cin >> l1 >> l2 >> l3 >> l4;
 
     if (l1 == l2 && l2 == l3 && l3 == l4){
-        cout << "Losangulo";
+        cout << "Losango";
     } else if(l1 == l2 && l3 == l4 || l2 == l3 && l1 == l4 || l2 == l4 && l1 == l3){
         cout << "Retangulo";
     } else if (l1 == l2 || l2 == l3 || l3==l4 || l1 == l3 || l1==l4 || l2 ==l4){

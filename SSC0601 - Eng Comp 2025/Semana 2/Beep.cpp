388dca73dbfa6a04025bdbc633f78274dd26196e
Matheus Valim <mvalim@valim.ovalim>
2025-04-04 08:35:32 -0300

    up beep

---
+++ SSC0601 - Eng Comp 2025/Semana 2/Beep.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int num;
+    cin >> num;
+
+    if (num > 1000){
+        std::printf("ALERTA!\a\n");
+    } else {
+        cout << "OK\n";
+    }
+}
